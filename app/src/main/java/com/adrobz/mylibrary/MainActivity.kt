c15574b32f322e2ec7212fdcdbc1967799ab628c
adrobz-alipervaiz <alipervaiz@adrobz.com>
2021-12-29 18:29:24 +0500

    Initial Commit

--- app/src/main/java/com/adrobz/mylibrary/MainActivity.kt
+++ app/src/main/java/com/adrobz/mylibrary/MainActivity.kt
@@ -2,6 +2,8 @@ package com.adrobz.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adrobz.mytoastlib.ToastLib
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- mytoastlib/src/main/java/com/adrobz/mytoastlib/ToastLibrary.kt
+++ mytoastlib/src/main/java/com/adrobz/mytoastlib/ToastLibrary.kt
@@ -3,7 +3,7 @@ package com.adrobz.mytoastlib
 import android.content.Context
 import android.widget.Toast
 
-class ToastLibrary {
+class ToastLib {
 
     fun myToast(mcx: Context, message:String){
         Toast.makeText(mcx,message,Toast.LENGTH_SHORT).show()
